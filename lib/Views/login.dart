506806ddb85a0b1b05f46c48a03dc2e0c3a4447f
Adam Czubernat <czuberad@sheridancollege.ca>
2021-03-24 00:42:24 -0400

    Implement UI for login page, display logo, with 2 text fields, with the password being obscured. as well as adding button to be used later to processes the login.

--- lib/Views/login.dart
+++ lib/Views/login.dart
@@ -5,8 +5,79 @@ class loginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return new Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
+      body: Container(
+        color: Colors.black,
+        child: Stack(
+          children: [
+            Positioned.fill(
+              child: Opacity(
+                opacity: 0.8,
+                child: Image.asset(
+                  'assets/signinBackground.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(color: Colors.transparent,height: 20),
+                  Image(image: AssetImage('assets/logo_picshare.png'),height: 300,width: 300),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child:TextField(
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintStyle: TextStyle(color: Colors.white),
+                        hintText: "Enter Email",
+                        filled: true,
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.deepOrangeAccent)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide(color: Colors.deepOrangeAccent)
+
+                        )
+
+                      ),
+                    ) ,
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(10),
+                    child:TextField(
+                      obscureText: true,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                          hintStyle: TextStyle(color: Colors.white),
+                          hintText: "Enter Password",
+                          filled: true,
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide(color: Colors.deepOrangeAccent)
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide(color: Colors.deepOrangeAccent)
+
+                          )
+
+                      ),
+                    ) ,
+                  ),
+                  RaisedButton(
+                      onPressed: (){
+                      },
+                      child: Text("LOGIN",style: TextStyle(color: Colors.black,fontSize: 20)),
+                    //color: Colors.black12,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
