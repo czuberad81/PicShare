aa5332fdf86751eb654ac39431172ec9c6df5064
Adam Czubernat <czuberad@sheridancollege.ca>
2021-04-17 16:19:10 -0400

    update previous commit

--- lib/Views/home_map.dart
+++ lib/Views/home_map.dart
@@ -9,15 +9,6 @@ import 'package:mobile_final_project/Styles/map.dart';
 class MyHomePage2 extends StatefulWidget {
   MyHomePage2({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,6 +45,8 @@ class _MyHomePage2State extends State<MyHomePage2> {
       width: MediaQuery.of(context).size.width,
 
       child: GoogleMap(
+        zoomGesturesEnabled: true,
+        zoomControlsEnabled: false,
         initialCameraPosition: CameraPosition(target:LatLng(43.661904723121374, -79.38172442298045),zoom: 10),
         onMapCreated: (GoogleMapController controller){
           controller.setMapStyle(mapS.styleMap);
@@ -63,29 +56,5 @@ class _MyHomePage2State extends State<MyHomePage2> {
 
     );
   }
-// Widget view(BuildContext context){
-//   Completer<GoogleMapController> _completer = Completer();
-//     return Container(
-//         height: MediaQuery.of(context).size.height,
-//         width: MediaQuery.of(context).size.width,
-//         child:Column(
-//           children: <Widget>[
-//             Align(
-//               alignment: FractionalOffset(0.0,0.05),
-//               child: IconButton(
-//               icon: Icon(Icons.account_circle_outlined,color: Colors.deepOrangeAccent,size: 30)
-//             ),
-//             ),
-//             GoogleMap(
-//               initialCameraPosition: CameraPosition(target:LatLng(43.661904723121374, -79.38172442298045),zoom: 10),
-//               onMapCreated: (GoogleMapController controller){
-//                 controller.setMapStyle(mapS.styleMap);
-//                 _completer.complete(controller);
-//               },
-//               )
-//           ],
-//         )
-//   );
-//
-// }
+
 }
